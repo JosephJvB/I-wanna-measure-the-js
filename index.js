157ab87c192b30e2ff467bf40527c8fd78b8dee4
JosephJvB <joevanbo@pm.me>
2019-04-06 12:34:00 +1300

    return bytes and chars

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 // https://github.com:
-// cheerio found 3 script urls, total length = 628189
+// cheerio found a compat-bootstrap .js that phantom did not!
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
 
 hey()
 async function hey () {
+  // store things
   const scriptUrls = []
-  let jabbaScrip = ''
+  let bigassString = ''
 
   const page = await fetch('https://github.com').then(r => r.text())
   const $ = cheerio.load(page)
 
   $('script').each((i, el) => {
     // scriptUrls.push($(el).attr('src')) also works but you have to do another parse
-    scriptUrls.push(el.attribs.src) // this better prob
+    scriptUrls.push(el.attribs.src) // this prob better
   })
 
   if(!scriptUrls.length) {
     throw new Error('no jabbascip found. MUH PROGRESSIVE WEB EXPERIENCE')
   }
+
+  // fetch js content from all found resource-urls
   const proms = scriptUrls.map(async s => {
-   const oneScriptyBoi = await fetch(s).then(r => r.text())
-   jabbaScrip += oneScriptyBoi
+    const singleyStringy = await fetch(s).then(r => r.text())
+    bigassString += singleyStringy
   })
   await Promise.all(proms)
-  console.log(jabbaScrip.length)
-  return jabbaScrip
+  
+  // https://nodejs.org/api/buffer.html#buffer_class_method_buffer_bytelength_string_encoding
+  console.log(
+    bigassString.length, // chars
+    Buffer.byteLength(bigassString, 'utf8') // bytes
+  )
+
+  return {
+    bytes: Buffer.byteLength(bigassString, 'utf8'),
+    chars: bigassString.length
+  }
 }
